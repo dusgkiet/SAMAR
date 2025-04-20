b7c4e41568dec50776a3dfbab8b6328ad3c8d634
dustin <cgkiet314@gmail.com>
2025-04-21 04:44:32 +0700

    styel[about, number, features]: Tiếp tục nâng cấp thêm style cho 3 phần này(#9)

--- js/index.js
+++ js/index.js
@@ -17,9 +17,9 @@ const arrService = [
     },
 ];
 
-let content = "";
+let contentS = "";
 for (let item of arrService) {
-    content += `
+    contentS += `
         <div class="service_content_item">
             <img src="${item.image}" alt="" />
             <h3>${item.title}</h3>
@@ -28,6 +28,38 @@ for (let item of arrService) {
     `
 };
 
-document.querySelector(".service_content").innerHTML = content;
+document.querySelector(".service_content").innerHTML = contentS;
 
 // Sự kiện cho Features
+const arrFeatures = [
+    {
+        icon: <i class="flaticon-idea"></i>,
+        title: "Idea & amp; Analysis Gathering",
+        content: "Maecenas laoreet efficitur sagittis. Aliquam eleifend nisl leo, sit amet consequat augue.",
+    },
+    {
+        icon: <i class="flaticon-coding"></i>,
+        title: "Design &amp; Developing",
+        content: "Maecenas laoreet efficitur sagittis. Aliquam eleifend nisl leo, sit amet consequat augue.",
+    },
+    {
+        icon: <i class="flaticon-rocket"></i>,
+        title: "Testing &amp; Lunching",
+        content: "Maecenas laoreet efficitur sagittis. Aliquam eleifend nisl leo, sit amet consequat augue.",
+    },
+];
+
+let contentF = "";
+for (let item of arrFeatures) {
+    contentF += `
+        <div class="features__bg">
+            ${item.icon}
+            <div>
+                <h3>${item.title}</h3>
+                <p>${item.content}</p>
+            </div>
+        </div>
+    `
+};
+
+document.querySelector(".features_content_left_group").innerHTML = contentF;
